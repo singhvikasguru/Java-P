5665f2924bbc2b9542c1f7e76e30bf419d4576d3
singhvikasguru <singhvikasguru@gmail.com>
2018-11-03 18:41:34 +0530

    third commit

--- Maximum tip calculator/src/tip.java
+++ Maximum tip calculator/src/tip.java
@@ -14,22 +14,53 @@ class tip {
 		    int n=sc.nextInt();
 		    int a[]=new int[n];
 		    int b[]=new int[n];
+		    int a2[][]=new int[n][2];
+		    int b2[][]=new int[n][2];
 		    int c=sc.nextInt();
 		    int d=sc.nextInt();
 		    int sum=0;
 		    for(int i=0;i<n;i++)
 		    {
 		        a[i]=sc.nextInt();
+		        a2[i][1]=i;
 		    }
 		    for(int i=0;i<n;i++)
 		    {
 		        b[i]=sc.nextInt();
+		        b2[i][1]=i;
 		    }
 		    for(int i=0;i<n;i++)
 		    {
 		        sum+=Math.max(a[i], b[i]);
+		        if(a[i]>b[i])
+		        {
+		            a2[i][0]=a[i]-b[i];
+		            b2[i][0]=-1;
+		        }
+		        else if(a[i]==b[i])
+		        {
+		            a2[i][0]=0;
+		            b2[i][0]=0;
+		        }
+		        else
+		        {
+		            b2[i][0]=b[i]-a[i];
+		            a2[i][0]=-1;
+		        }
+		    }
+		    Arrays.sort(a2, (a3, b3) -> Integer.compare(a3[0], b3[0]));
+		    Arrays.sort(b2, (a4, b4) -> Integer.compare(a4[0], b4[0]));
+		    for(int i=0;i<n;i++)
+		    {
+		        System.out.println(a2[i][0]+" "+a2[i][1]);
+		    }
+		    for(int i=0;i<n;i++)
+		    {
+		        System.out.println(b2[i][0]+" "+b2[i][1]);
 		    }
 		    System.out.println(sum);
+		    
+		    
 		}
 	}
 }
